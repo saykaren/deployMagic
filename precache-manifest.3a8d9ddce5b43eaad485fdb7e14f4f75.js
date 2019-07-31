736745eb45cadcc26fccf72bd8c9568a457cfa84
Karen Garcia <karen.garcia@nexscreen.com>
2019-07-30 21:23:54 -0600

    Updates

---
+++ precache-manifest.3a8d9ddce5b43eaad485fdb7e14f4f75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a253488480c4c2d92d0dfec570f23d",
+    "url": "/deployMagic/index.html"
+  },
+  {
+    "revision": "437f2af79762eab810d4",
+    "url": "/deployMagic/static/css/main.05529f43.chunk.css"
+  },
+  {
+    "revision": "5b78da2dc9bb2a847546",
+    "url": "/deployMagic/static/js/2.631e91b0.chunk.js"
+  },
+  {
+    "revision": "437f2af79762eab810d4",
+    "url": "/deployMagic/static/js/main.9a095848.chunk.js"
+  },
+  {
+    "revision": "a3d9ed31878956504c7c",
+    "url": "/deployMagic/static/js/runtime~main.e5d9211e.js"
+  },
+  {
+    "revision": "6704307ad28acf81202b0801ec841261",
+    "url": "/deployMagic/static/media/alanna_heroPic.6704307a.jpg"
+  },
+  {
+    "revision": "efd8a1fa8218be31598ed9d815697dd4",
+    "url": "/deployMagic/static/media/reference_Karen.efd8a1fa.jpg"
+  },
+  {
+    "revision": "4ee98378ff2cd65d37dc47e7b6cd0023",
+    "url": "/deployMagic/static/media/sayKaren_Logo_transparent_7.2019.4ee98378.png"
+  }
+]);
